040ad1ddb1b5403ba0c49d1b4482947b649e95dc
Miguel Angel Alvarez <malvarez@desarrolloweb.com>
2019-05-06 15:21:35 +0200

    LitElement migration

--- dile-button.js
+++ dile-button.js
@@ -1,16 +1,17 @@
-import { PolymerElement } from '@polymer/polymer/polymer-element.js';
-import { html } from '@polymer/polymer/lib/utils/html-tag.js';
+import { LitElement, html, css } from 'lit-element';
+
 /**
  * # `dile-button`
  *
- * Polymer Web Component to implement a simple animated button
+ *  Web Component to implement a simple animated button, based on LitElement
  *
  * `<dile-button>`
  *
- * This components accepts a "role" property to styling.
- * Also a boolean property called "disabled" to disable the button.
+ * This components accepts a "role" property to styling. Accepted values are: undefined, primary, danger, warning, success
+ * 
+ * Implements a boolean property called "disabled" to disable the button.
  *
- * Clicks on the button produces a CSS animation, when the button disabled property is false.
+ * Clicks on the button produces a CSS animation, but only when the button disabled property is false.
  *
  * ```html
  * <dile-button
@@ -22,50 +23,55 @@ import { html } from '@polymer/polymer/lib/utils/html-tag.js';
  * Custom property | Description | Default
  * ----------------|-------------|---------
  * --dile-button-padding | Padding applied to the buttton box | 10px
- * --dile-button-box | Mixind applied to the button box | {}
+ * --dile-button-border-width | Border applied to the button box | 0
+ * --dile-button-boder-color | Border color applied to the button box | transparent 
  *
  * @customElement
  * @polymer
  * @demo demo/index.html
  */
-class DileButton extends (class extends PolymerElement {}) {
-  static get template() {
-    return html`
-    <style>
+
+class DileButton extends LitElement {
+
+  static get styles() {
+    return css`
+      * {
+        box-sizing: border-box;
+      }
       :host {
-        display: inline;
-        user-select: none
+        display: inline-block;
+        user-select: none;
       }
       .undefined {
         background-color: #868e96;
       }
       .primary {
-        background-color: #007bff
+        background-color: #007bff;
       }
       .danger {
-        background-color: #dc3545
+        background-color: #dc3545;
       }
       .success {
-        background-color: #28a745        
+        background-color: #28a745;       
       }
       .warning {
-        background-color: #ffc107        
+        background-color: #ffc107;       
       }
       div {
+        display: inline-block;
         color: #fff;
+        background-color: #888;
         border-radius: 5px;
-        border: none;
+        border: var(--dile-button-border-width) solid var(--dile-button-border-color);
         padding: var(--dile-button-padding, 10px);
         cursor:pointer;
         outline:none;
-        display: inline-block;
         -webkit-animation-duration: 0.3s; 
         animation-duration: 0.3s; 
         -webkit-animation-fill-mode: both; 
         animation-fill-mode: both; 
         -webkit-animation-timing-function: ease-in; 
         animation-timing-function: ease-in;
-        @apply --dile-button-box;
       }
       div:active{ 
         -webkit-animation-name: anim; 
@@ -76,75 +82,49 @@ class DileButton extends (class extends PolymerElement {}) {
         10%, 30% {transform: scale(0.9) rotate(-2deg);}  
         100% {transform: scale(1) rotate(0);} 
       } 
-      .disabled {
+      div.disabled {
         opacity: 0.5;
       }
-      .disabled:active {
+      .disabled:active {
+        -webkit-animation-name: none; 
         animation-name: none;
       }
-      div:active{ 
-        -webkit-animation-name: anim; 
-        animation-name: anim; 
-      }
-
-    </style>
-    <div on-click="_onClick" class\$="[[cssClass]]">
-      [[label]]
-    </div>
-`;
+    `; 
+  }
+  render() {
+    return html`
+      <div @click="${this._onClick}" class="${this._getClass(this.role, this.disabled)}">
+        <slot></slot>
+      </div>
+    `;
   }
 
-  static get is() { return 'dile-button'; }
   static get properties() {
     return {
-      /**
-      * The text displayed in the button
-      * @type {String}
-      */
-      label: {
-        type: String,
-        value: 'dile-button'
-      },
-
       /**
       * The role of the button. This property configures the button color
       * Values accepted: 'undefined', 'primary', 'danger', 'warning', 'success'.
       * Default: 'undefined'.
       * @type {String}
       */
-      role: {
-        type: String,
-        value: 'undefined'
-      },
-
-      /**
-      * The computed CSS Class.
-      * @type {Boolean}
-      */
-      cssClass: {
-        type: String,
-        computed: '_changeClass(role, disabled)'
-      },
+      role: { type: String },
 
       /**
       * Disabled property disables the button (disabled buttons stops click propagation events).
       * @type {Boolean}
       */
-      disabled: {
-        type: Boolean,
-        value: false
-      }
+      disabled: { type: Boolean }
     };
   }
 
-  _changeClass(role, disabled) {
-    let newClass = role;
-    if (disabled) {
-      newClass += ' disabled';
-    }
-    return newClass;
+  constructor() {
+    super();
+    this.role = 'undefined';  
   }
 
+  _getClass(role, disabled) {
+    return role + (disabled ? ' disabled' : '');
+  }
 
   _onClick(e) {
     if(this.disabled) {
@@ -152,5 +132,4 @@ class DileButton extends (class extends PolymerElement {}) {
     }
   }
 }
-
-window.customElements.define(DileButton.is, DileButton);
+customElements.define('dile-button', DileButton);
